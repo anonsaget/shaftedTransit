b0b7cd35efe793ee030bfe7c7a27e86bbdb4e8d1
anonsaget <92192963+anonsaget@users.noreply.github.com>
2022-12-10 16:05:22 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,86 +4,43 @@ var ticketColorInput;
 var zones;
 var fromColor;
 
+const button = document.querySelector('input[type="button"][value="Generate"]');
+
+function hexToRgb(hex) {
+    // Convert the hexadecimal string to a base-10 integer
+    const value = parseInt(hex, 16);
+  
+    // Get the red, green, and blue values
+    const r = (value >> 16) & 0xff;
+    const g = (value >> 8) & 0xff;
+    const b = value & 0xff;
+  
+    // Return the RGB values as an array
+    return [r, g, b];
+  }
+
 function onClick(){
-	//Get name or Interstate
-	fromWhere = document.getElementById("fromInput").value; 
-	zones = document.getElementById("zoneInput").value; 
-	ticketColorInput = document.getElementById("ticketColorInput").value; 
 
+	//Loading new page
+	fromWhere = document.getElementById("interstate-intrastate").value; 
+	zones = document.getElementById("zones").value; 
+	ticketColorInput = document.getElementById("ticket-color").value; 
+	var barColor1 = document.getElementById("color1").value;
+	var barColor2 = document.getElementById("color2").value;
+	var barColor3 = document.getElementById("color3").value;
+
+	colorArr[0] = barColor1.replace('#', '');
+	colorArr[1] = barColor2.replace('#', '');
+	colorArr[2] = barColor3.replace('#', '');
+	console.log(colorArr[0])
 	//Get colors
-	var inputColors = document.getElementsByClassName('clr-field');
-	for(i = 0; i < inputColors.length; i++) {
-	  colorArr[i] = inputColors[i].style.color;
-	}
-	fromColor = "rgb(0,0,0)"
+	fromColor = "000000"
 	fromWhere = fromWhere.toUpperCase();
+
 	if(fromWhere == 'INTERSTATE')
-		fromColor = "rgb(255,0,0)"
-	
+		fromColor = "FF0000"
+	//document.getElementById("spinner-border text-light").style.visibility = "visible"
+	  
+	location.assign(`generated.html?fromWhere=${fromWhere}&fromColor=${fromColor}&zones=${zones}&ticketColor=${ticketColorInput}&color1=${colorArr[0]}&color2=${colorArr[1]}&color3=${colorArr[2]}`);
 
-	const params = new URLSearchParams({
-			fromWhereA: fromWhere,
-			fromWhereCo: fromColor,
-			zoneNumA: zones,
-			ticketColorA: ticketColorInput,
-			color1A: colorArr[0],
-			color2A: colorArr[1],
-			color3A: colorArr[2]
-		});
-	var separator = (window.location.href.indexOf("?")===-1)?"?":"&";
-	window.location.href = "generated.html" + separator + params;
-
-}
-
-function setCookie(name,value,days) {
-    var expires = "";
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days*24*60*60*1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
-}
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for(var i=0;i < ca.length;i++) {
-        var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
-    }
-    return null;
-}
-
-function deleteAllCookies() {
-    var cookies = document.cookie.split(";");
-
-    for (var i = 0; i < cookies.length; i++) {
-        var cookie = cookies[i];
-        var eqPos = cookie.indexOf("=");
-        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-    }
-}
-
-window.onload = function () { 
-	window.scrollTo(0,1);
-	let here = new URL(window.location.href).searchParams;
-	console.log(window.location.href);
-
-	document.getElementById('fromWhere').innerHTML = here.get('fromWhereA');
-	document.getElementById('fromWhere').style.color = here.get('fromWhereCo');
-	document.getElementById('zoneNum').innerHTML = here.get('zoneNumA');
-	document.getElementById('ticketColor').src = "img/" + here.get('ticketColorA') + ".png";
-	document.getElementById('coloredBoxes1').style.backgroundColor = here.get('color1A');
-	document.getElementById('coloredBoxes2').style.backgroundColor = here.get('color2A');
-	document.getElementById('coloredBoxes3').style.backgroundColor = here.get('color3A');
-
-
-	
-	const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
-	if (!isInStandaloneMode()) {
-		this.setState({ showInstallMessage: true });
-	  }
 }
